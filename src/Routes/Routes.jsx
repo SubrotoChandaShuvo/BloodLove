e781c026aaeaccee7c8a28fe39632a5bb8d5d4bb
Subroto Chanda Shuvo <subrotochandashuvo@gmail.com>
2025-12-13 21:53:42 +0600

    Blood love page created

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../RootLayout/RootLayout";
+import Home from "../Pages/Home/Home";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout/>,
+    children:[
+        {
+            path:'/',
+            Component: Home
+        }
+    ]
+  },
+]);
+
+export default router;
